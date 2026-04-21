9fa542c12e398c083a7b6363f1cad327fcaadff6
Olamixx <ifeolayeni97@gmail.com>
2026-04-21 11:44:48 +0100

    added missing dotenv package to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,4 @@
-import dotenv from "dotenv"
-dotenv.config() // Load environment variables from .env file  
+import "dotenv/config";
 import http from "http"
 import express from "express"
 import cors from "cors"
